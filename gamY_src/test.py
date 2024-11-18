e2966c6a788db0a2990201ef5e61ab330523c4a0
Peter <psp@dreamgruppen.dk>
2024-11-18 12:19:22 +0100

    Simplifying test

--- gamY_src/test.py
+++ gamY_src/test.py
@@ -1,19 +1,4 @@
-import sys
-import shutil
-import os
-
 import dreamtools as dt
-dt.gamY.require_variable_with_equation = True
-dt.gamY.block_equations_suffix = "_equations"
-dt.gamY.automatic_dummy_suffix = "_exists_dummy"
-
-## Set local paths
-root = dt.find_root()
-sys.path.insert(0, root)
-#import paths
-
-## Set working directory
-os.chdir(fr"{root}/gamY_src")
 
 dt.gamY.run("test.gms")
 
