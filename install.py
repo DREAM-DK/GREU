881d4e50c51af287acbec948035dbd85fc00cfa0
Martin Kirk Bonde <martin@bonde.dk>
2024-12-02 23:14:24 +0100

    Adds gamsapi with version number and nbformat to install.py

--- install.py
+++ install.py
@@ -4,5 +4,5 @@ import subprocess
 # Install python modules in python installation that comes with GAMS
 subprocess.run([
     sys.executable, "-m", "pip", "install", "--upgrade",
-    "dream-tools==3.0.0", 
+    "dream-tools==3.0.0", "gamsapi[all]==46.5.0", "nbformat"
 ], check=True)
