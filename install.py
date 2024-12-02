989a8889cbb9ed9e9b2e10f369bc166c94614595
Martin Kirk Bonde <martin@bonde.dk>
2024-12-02 23:07:31 +0100

    Remove pip installation from install.py

--- install.py
+++ install.py
@@ -1,10 +1,6 @@
 import sys
 import subprocess
 
-# Install pip in python installation that comes with GAMS
-subprocess.run(["curl", "https://bootstrap.pypa.io/get-pip.py", "-o", "get-pip.py"], check=True)
-subprocess.run([sys.executable, "get-pip.py"], check=True)
-
 # Install python modules in python installation that comes with GAMS
 subprocess.run([
     sys.executable, "-m", "pip", "install", "--upgrade",
