5d1d96afc2cecf46e798a4f9eec73695034598b1
akgdreammodel <akg@dreamgruppen.dk>
2024-12-03 10:48:06 +0100

    Comment out paths for server-use

--- gamY_src/run.py
+++ gamY_src/run.py
@@ -10,7 +10,7 @@ dt.gamY.automatic_dummy_suffix = "_exists_dummy"
 ## Set local paths
 root = dt.find_root()
 sys.path.insert(0, root)
-import paths
+# import paths
 
 ## Set working directory
 os.chdir(fr"{root}/gamY_src")
