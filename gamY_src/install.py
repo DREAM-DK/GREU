0ff9d0d5a6d6cbe4f5c52aff82981a7f16cdd581
Louisstewart21 <lbs@dreamgruppen.dk>
2025-04-03 14:31:17 +0200

    update install.py

--- gamY_src/install.py
+++ gamY_src/install.py
@@ -4,31 +4,8 @@ import subprocess
 # Install python modules in python installation that comes with GAMS
 subprocess.run([
     sys.executable, "-m", "pip", "install", "--upgrade",
-    "dream-tools==3.0.0", "gamsapi[all]==46.5.0", "nbformat", 
+    "dream-tools==3.0.0", 
     "gamspy",
+    "matplotlib",
 ], check=True)
 
-
-# # Install python modules in python installation that comes with GAMS
-# subprocess.run([
-#     "C:/GAMS/46/GMSPython/Scripts/pip", "install", "--upgrade",
-#     "gamspy==1.5.1",
-# ], check=True)
-
-# subprocess.run(["C:/GAMS/46/GMSPython/Scripts/gamsPy", "install", "license", "Q:/Installation/GAMS/DREAM_GAMSPy_license.txt"], check=True)
-
-# set GAMSDIR=C:/GAMS/46
-# set gamsPy=%GAMSDIR%/GMSPython/Scripts/gamsPy
-# C:/GAMS/46/GMSPython/Scripts/pip
-# %gamsPy% install license Q:\Installation\GAMS\DREAM_GAMSPy_license.txt
-
-# C:/GAMS/46/GMSPython/Scripts/gamsPy install license Q:\Installation\GAMS\DREAM_GAMSPy_license.txt
-
-# Install python modules in python installation that comes with GAMS
-# subprocess.run([
-#     sys.executable, "-m", "pip", "install", "--upgrade",
-#     "dream-tools==3.0.0",
-#     "gamspy", 
-#     # "numpy", "scipy", "statsmodels", "xlwings",
-#     # "plotly", "kaleido==0.1.0.post1", "xhtml2pdf", "dataframe_image", "pyhtml2pdf", "PyPDF2",
-# ], check=True)
