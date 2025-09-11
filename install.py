a5f25bf6ff302cf996efdd949e5bb6f2bd26af43
Louisstewart21 <lbs@dreamgruppen.dk>
2025-09-11 09:34:22 +0200

    update install.py

--- install.py
+++ install.py
@@ -4,5 +4,9 @@ import subprocess
 # Install python modules in python installation that comes with GAMS
 subprocess.run([
     sys.executable, "-m", "pip", "install", "--upgrade",
-    "dream-tools==3.0.0", "gamsapi[all]==47.6.0", "nbformat","gamspy==1.0.3"
+    "dream-tools==3.0.0", 
+    "gamsapi[all]==49.5.0", 
+    "nbformat",
+    "gamspy==1.10.0",
+    "matplotlib",
 ], check=True)
