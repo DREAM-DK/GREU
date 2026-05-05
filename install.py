55143e66ac8ce60cefe60f364fa1f8ee6b8c2d00
Karl Noergaard <karnor@dreamgruppen.dk>
2026-05-05 09:39:40 +0200

    run prepreocessing + include version control for parquet

--- install.py
+++ install.py
@@ -4,11 +4,12 @@ import subprocess
 # Install python modules in python installation that comes with GAMS
 subprocess.run([
     sys.executable, "-m", "pip", "install", "--upgrade",
-    "dream-tools==3.0.0", 
+    "dream-tools==3.0.0",
+    "pandas==3.0.2", 
     "gamsapi[all]", 
     "nbformat",
     "gamspy==1.21.0",
-    "pyarrow",
+    "pyarrow==24.0.0",
     "matplotlib",
     "eurostat",
 ], check=True)
