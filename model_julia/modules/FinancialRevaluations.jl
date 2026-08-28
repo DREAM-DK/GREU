0da56135dcfe95af1e546edf7eb06b0f596ef54d
Martin Kirk Bonde <martin@bonde.dk>
2026-08-28 22:18:21 +0200

    Fixes issues in FinancialRevaluations caused by changing "Equity" to include derivatives for which goverment and households also have small liabilities

--- model_julia/modules/FinancialRevaluations.jl
+++ model_julia/modules/FinancialRevaluations.jl
@@ -19,13 +19,20 @@ import ..Tags: ForecastZero
 import ..Time: t, t1, T
 
 # ============================================================================
+# Indices
+# ============================================================================
+const equity_issuer = [:NonFinCorp, :FinCorp, :RoW]
+# Households and Government issue a small value of derivatives,#
+# which we lump with equity in the data, and mostly ignore
+
+# ============================================================================
 # Variables
 # ============================================================================
 
 const FinancialRevaluationsTag = Tag(:FinancialRevaluations)
 
 @variables model :: FinancialRevaluationsTag begin
-  rFirmRequiredReturn_s[(s,t)=vFinPosition_s_f[:,:Equity,:Liab,:]], "Required nominal equity return by issuer."
+  rFirmRequiredReturn_s[s=equity_issuer, t=t], "Required nominal equity return by issuer."
   rFinReval_s_f[(s,f,al,t)=vFinPosition_s_f] :: ForecastZero, "Financial revaluation rate by position."
   rFinReval_f[f=fin_instrument, t=t], "Average financial revaluation rate by instrument."
 end
@@ -74,12 +81,12 @@ function define_equations()
     rFinReval_s_f[s,f,al,t] == rFinReval_f[f,t]
 
     # Firm value is the present value of future dividends less equity issues.
-    rFinReval_s_f[s=sector, f=[:Equity], al=[:Liab], t=t1:(T-1)],
+    rFinReval_s_f[s=equity_issuer, f=[:Equity], al=[:Liab], t=t1:(T-1)],
     vEquity[s,t] * (1+rFirmRequiredReturn_s[s,t+1]) ==
     vDividends[s,t+1]*fv - vEquityIssues[s,t+1]*fv + vEquity[s,t+1]*fv
 
     # Terminal value. Equation is left out of static calibration (T>t1).
-    rFinReval_s_f[s=sector, f=[:Equity], al=[:Liab], t=[T]; T > t1],
+    rFinReval_s_f[s=equity_issuer, f=[:Equity], al=[:Liab], t=[T]; T > t1],
     vEquity[s,t] * (1+rFirmRequiredReturn_s[s,t]) == vDividends[s,t]*fv - vEquityIssues[s,t]*fv + vEquity[s,t]*fv
   end
 
@@ -93,13 +100,16 @@ end
 function define_calibration()
   block = define_equations() + @block model begin
     # Keep required returns constant after t1+1.
-    rFirmRequiredReturn_s[s=sector, t=(t1+2):T],
+    rFirmRequiredReturn_s[s=equity_issuer, t=(t1+2):T],
     rFirmRequiredReturn_s[s,t] == rFirmRequiredReturn_s[s,t1+1]
   end
 
   @endo_exo_swap! block begin
     rFinReval_s_f[s=sector, f=[:Debt], al=ass_liab, t=[t1]],
     vFinReval_s_f[s=sector, f=[:Debt], al=ass_liab, t=[t1]]
+
+    rFinReval_s_f[s=sector, f=[:Equity], al=[:Liab], t=[t1]; s ∉ equity_issuer],
+    vFinReval_s_f[s=sector, f=[:Equity], al=[:Liab], t=[t1]; s ∉ equity_issuer]]
 
     residual(rFinReval_s_f)[s=sector, f=[:Equity], al=[:Assets], t=[t1]],
     vFinReval_s_f[s=sector, f=[:Equity], al=[:Assets], t=[t1]]
@@ -108,16 +118,16 @@ function define_calibration()
   # Static calibration only.
   if T == t1
     @endo_exo_swap! block begin
-      rFinReval_s_f[s=sector, f=[:Equity], al=[:Liab], t=[t1]],
-      vFinReval_s_f[s=sector, f=[:Equity], al=[:Liab], t=[t1]]
+      rFinReval_s_f[s=equity_issuer, f=[:Equity], al=[:Liab], t=[t1]],
+      vFinReval_s_f[s=equity_issuer, f=[:Equity], al=[:Liab], t=[t1]]
     end
   end
 
   # Dynamic calibration only. The t1 valuation uses rFirmRequiredReturn_s[t1+1].
   if T > t1
     @endo_exo_swap! block begin
-      rFirmRequiredReturn_s[s=sector,t1+1],
-      rFinReval_s_f[s=sector,:Equity,:Liab,t1]
+      rFirmRequiredReturn_s[s=equity_issuer,t1+1],
+      vFinReval_s_f[s=equity_issuer,:Equity,:Liab,t1]
     end
   end
 
